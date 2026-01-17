5697f517910f89975e235f4cfbd20e378f8e9260
shirne <shirne@126.com>
2026-01-17 11:31:19 +0800

    改进border text

--- lib/src/widgets/bordered_text.dart
+++ lib/src/widgets/bordered_text.dart
@@ -1,33 +1,74 @@
 import 'package:flutter/material.dart';
 
 class BorderedText extends StatelessWidget {
-  const BorderedText(
-    this.text, {
+  const BorderedText({
     super.key,
-    this.style = const TextStyle(),
-    required this.borderColor,
+    required this.text,
+    this.borderColor,
+    this.borderGradient,
     this.borderWidth = 2,
-  });
+    this.strokeCap,
+    this.strokeJoin,
+  }) : assert(borderColor != null || borderGradient != null);
 
-  final String text;
-  final TextStyle style;
-  final Color borderColor;
+  BorderedText.text(
+    String text, {
+    super.key,
+    TextStyle? style,
+    this.borderColor,
+    this.borderGradient,
+    this.borderWidth = 2,
+    this.strokeCap,
+    this.strokeJoin,
+  }) : assert(borderColor != null || borderGradient != null),
+       text = Text(text, style: style);
+
+  final Text text;
+  final Color? borderColor;
+  final Gradient? borderGradient;
   final double borderWidth;
+  final StrokeCap? strokeCap;
+  final StrokeJoin? strokeJoin;
 
   @override
   Widget build(BuildContext context) {
+    Widget bgText = Text(
+      text.data ?? '',
+      strutStyle: text.strutStyle,
+      textAlign: text.textAlign,
+      textDirection: text.textDirection,
+      locale: text.locale,
+      softWrap: text.softWrap,
+      overflow: text.overflow,
+      textScaler: text.textScaler,
+      maxLines: text.maxLines,
+      //semanticsLabel: text.semanticsLabel,
+      //semanticsIdentifier: text.semanticsIdentifier,
+      textWidthBasis: text.textWidthBasis,
+      textHeightBehavior: text.textHeightBehavior,
+      //selectionColor: text.selectionColor,
+      style: (text.style ?? TextStyle()).copyWith(
+        foreground: Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = borderWidth + 1
+          ..strokeCap = strokeCap ?? StrokeCap.round
+          ..strokeJoin = strokeJoin ?? StrokeJoin.round
+          ..color = borderColor ?? Colors.black,
+      ),
+    );
+    if (borderGradient != null) {
+      bgText = ShaderMask(
+        shaderCallback: (bounds) {
+          return borderGradient!.createShader(bounds);
+        },
+        blendMode: BlendMode.srcIn,
+        child: bgText,
+      );
+    }
     return Stack(
       children: [
-        Text(
-          text,
-          style: style.copyWith(
-            foreground: Paint()
-              ..style = PaintingStyle.stroke
-              ..strokeWidth = borderWidth + 1
-              ..color = borderColor,
-          ),
-        ),
-        Text(text, style: style),
+        IgnorePointer(ignoring: true, child: bgText),
+        text,
       ],
     );
   }
